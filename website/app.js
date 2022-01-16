43297fd9912e21fc0307c43a6e437143af4cb59d
TsaiTing Wang <tsaiting.wang@fleetcor.com>
2022-01-16 09:13:30 -0500

    fix date value, add the zip validation, and add package.json as suggested

--- website/app.js
+++ website/app.js
@@ -2,9 +2,10 @@
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth()+1+'.'+ d.getDate()+'.'+ d.getFullYear();
 
 // Personal API Key for OpenWeatherMap API
+// Directives &units=imperial for Fahrenheit or &units=metric for celsius is added in our final URL formed for API calls.
 const apiKey = '&appid=8352ebdbd0ef002753e1b61f4c02256f&units=imperial';
 let baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip='
 
@@ -13,15 +14,21 @@ document.getElementById('generate').addEventListener('click', performAction);
 
 /* Function called by event listener */
 function performAction(e){
-    const newZip     =  document.getElementById('zip').value + ",us";
-    const Feeling    =  document.getElementById('feelings').value;
-    getZip(baseURL,newZip, apiKey)
-    .then(function(data){
-        const temp = data.main.temp;
-        //console.log(temp);
-        postData('/addHistory', {temperature: temp, feeling: Feeling, date:newDate})
-        updateUI()
-    })
+    const isValid = /^[0-9]{5}(?:-[0-9]{4})?$/.test(document.getElementById('zip').value);
+    if (isValid) {
+        const newZip     =  document.getElementById('zip').value + ",us";
+        const Feeling    =  document.getElementById('feelings').value;
+        getZip(baseURL,newZip, apiKey)
+        .then(function(data){
+            const temp = data.main.temp;
+            //console.log(temp);
+            postData('/addHistory', {temperature: temp, feeling: Feeling, date:newDate})
+            updateUI()
+        })
+    }
+    else {
+        alert('Please enter a valid zip first');
+    }
 }
 
 /* Function to GET Web API Data*/
